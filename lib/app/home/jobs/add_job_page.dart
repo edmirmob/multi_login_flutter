3cbe532e0a9603fcb9ce2cbc0280a4802aff116c
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-03 21:38:30 +0200

    Validating and saving Form data

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -13,52 +13,79 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name;
+  int _ratePerHour;
+  bool validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit() {
+    if (validateAndSaveForm()) {
+      print('form saved with name:  $_name and rate: $_ratePerHour');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
         title: Text('New Job'),
+        actions: [
+          TextButton(
+            onPressed: _submit,
+            child: Text('Save',
+                style: TextStyle(fontSize: 18, color: Colors.white)),
+          ),
+        ],
       ),
       body: _buildContents(),
       backgroundColor: Colors.grey[200],
     );
   }
-}
 
-Widget _buildContents() {
-  return SingleChildScrollView(
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: _buildForm(),
+  Widget _buildContents() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: _buildForm(),
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget _buildForm() {
-  return Form(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: _buildFormChildren(),
-    ),
-  );
-}
-
-List<Widget> _buildFormChildren() {
-  return [
-    TextFormField(
-      decoration: InputDecoration(labelText: 'Job name'),
-    ),
-    TextFormField(
-      decoration: InputDecoration(
-        labelText: 'Rate per hour',
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildFormChildren(),
       ),
-      keyboardType: TextInputType.numberWithOptions(),
-    ),
-  ];
+    );
+  }
+
+  List<Widget> _buildFormChildren() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Job name'),
+        validator: (value)=>value.isNotEmpty ? null : 'Name can\'t be empty',
+        onSaved: (value) => _name = value,
+      ),
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Rate per hour'),
+        onSaved: (value)=>_ratePerHour = int.tryParse(value),
+        keyboardType: TextInputType.numberWithOptions(),
+      ),
+    ];
+  }
 }
