a3278fe08eb4652f07266d294c8954cb407e352f
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-08 11:29:16 +0200

    Adding a CupertinoTabScaffold

--- lib/app/home/cupertino_home_scafold.dart
+++ lib/app/home/cupertino_home_scafold.dart
@@ -1,17 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_login_flutter/app/home/tab_item.dart';
 
 class CupertinoHomeScafold extends StatelessWidget {
   const CupertinoHomeScafold({
     Key key,
-   @required this.currentTab,
-   @required this.onSelectTab,
+    @required this.currentTab,
+    @required this.onSelectTab,
   }) : super(key: key);
   final TabItem currentTab;
   final ValueChanged<TabItem> onSelectTab;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          _buildItem(TabItem.jobs),
+          _buildItem(TabItem.entries),
+          _buildItem(TabItem.accounts),
+        ],
+        onTap: (index) => onSelectTab(
+          TabItem.values[index],
+        ),
+      ),
+      tabBuilder: (context, index) {},
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem itemTab) {
+    final tabData = TabItemData.allTabs[itemTab];
+    final color = currentTab == itemTab ? Colors.indigo : Colors.grey;
+    return BottomNavigationBarItem(
+      icon: Icon(tabData.icon, color: color),
+      label: tabData.label,
+    );
   }
 }
--- lib/app/home/tab_item.dart
+++ lib/app/home/tab_item.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
 enum TabItem {jobs, entries,accounts }
+
+class TabItemData{
+  
+ const TabItemData({@required this.label, @required this.icon});
+
+  final String label;
+  final IconData icon;
+
+  static const Map<TabItem,TabItemData> allTabs = {
+    TabItem.jobs: TabItemData(label: 'Jobs', icon: Icons.work),
+    TabItem.entries: TabItemData(label: 'Entries', icon: Icons.view_headline),
+    TabItem.accounts: TabItemData(label: 'Accounts', icon: Icons.person),
+  };
+
+}
