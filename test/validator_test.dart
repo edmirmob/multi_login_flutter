aff786f917ad733ecd3425a9541983506b3839a2
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-09 22:49:18 +0200

    Writing the first unit test

---
+++ test/validator_test.dart
@@ -0,0 +1,17 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:multi_login_flutter/app/sign_in/validators.dart';
+
+void main() {
+  test('non empty string', () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid('test'), true);
+  });
+}
