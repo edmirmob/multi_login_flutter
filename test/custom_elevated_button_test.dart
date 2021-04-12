94efb32f8537cfbc6afe5c20abd5d3da53e36823
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-12 14:57:43 +0200

    Finding widgets and matcher arguments

--- test/custom_elevated_button_test.dart
+++ test/custom_elevated_button_test.dart
@@ -3,6 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:multi_login_flutter/common_widgets/custom_elevated_button.dart';
 void main(){
   testWidgets('', (WidgetTester tester)async{
-await tester.pumpWidget(MaterialApp(home: CustomElevatedButton()));
+await tester.pumpWidget(MaterialApp(home: CustomElevatedButton(
+  child: Text('tap me'),
+)));
+final button = find.byType(ElevatedButton);
+expect(button, findsOneWidget);
+expect(find.byType(SizedBox), findsNothing);
+expect(find.text('tap me'), findsOneWidget);
   });
 }
