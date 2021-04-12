d843d38eef7f8d68ccfa526c30ce0361fa2616fd
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-12 15:06:30 +0200

    Testing widget callbacks

--- test/custom_elevated_button_test.dart
+++ test/custom_elevated_button_test.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:multi_login_flutter/common_widgets/custom_elevated_button.dart';
 void main(){
-  testWidgets('', (WidgetTester tester)async{
+  var pressed = false;
+  testWidgets('onpressed callBack', (WidgetTester tester)async{
 await tester.pumpWidget(MaterialApp(home: CustomElevatedButton(
   child: Text('tap me'),
+  onPressed:()=> pressed = true,
 )));
 final button = find.byType(ElevatedButton);
 expect(button, findsOneWidget);
-expect(find.byType(SizedBox), findsNothing);
+expect(find.byType(TextButton), findsNothing);
 expect(find.text('tap me'), findsOneWidget);
+await tester.tap(button);
+expect(pressed, true);
   });
 }
