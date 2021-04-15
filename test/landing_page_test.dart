2a72c6210aa60e21497fe665ab89df1e5153fbf1
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-15 11:42:02 +0200

    Using widget tests with StreamBuilder

---
+++ test/landing_page_test.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:multi_login_flutter/app/home/home_page.dart';
+import 'package:multi_login_flutter/app/landing_page.dart';
+import 'package:multi_login_flutter/app/sign_in/sign_in_page.dart';
+import 'package:multi_login_flutter/services/auth.dart';
+import 'package:provider/provider.dart';
+
+class MockAuth extends Mock implements AuthBase {}
+
+void main() {
+  MockAuth mockAuth;
+   StreamController<UserId> onAuthStateChangedController;
+
+  setUp(() {
+    mockAuth = MockAuth();
+     onAuthStateChangedController = StreamController<UserId>();
+  });
+
+  tearDown(() {
+    onAuthStateChangedController.close();
+  });
+
+  Future<void> pumpLandingPage(WidgetTester tester,
+      ) async {
+    await tester.pumpWidget(
+      Provider<AuthBase>(
+        create: (_) => mockAuth,
+        child: MaterialApp(
+          home: LandingPage(),
+        ),
+      ),
+    );
+    await tester.pump();
+  }
+  void stubOnAuthStateChangedYields(Iterable<UserId>onAuthStateChanged){
+    onAuthStateChangedController.addStream(Stream<UserId>.fromIterable(onAuthStateChanged));
+    when(mockAuth.onAuthStateChanged)
+    .thenAnswer((_){
+   return onAuthStateChangedController.stream;
+    });
+  }
+
+  testWidgets('stream waiting', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('null user', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([null]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(SignInPage), findsOneWidget);
+  });
+
+  testWidgets('non-null user', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([UserId(uid: '123456')]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+}
