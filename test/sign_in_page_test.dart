85f5c7daf13264bd2937367eef97afd3509cfef6
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-15 12:25:21 +0200

    Testing navigation

--- test/sign_in_page_test.dart
+++ test/sign_in_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:multi_login_flutter/app/sign_in/sign_in_page.dart';
 import 'package:multi_login_flutter/services/auth.dart';
 import 'package:provider/provider.dart';
@@ -8,9 +9,11 @@ import 'mocks.dart';
 
 void main() {
   MockAuth mockAuth;
+ MockNavigatorObserver mockNavigatorObserver;
 
   setUp(() {
     mockAuth = MockAuth();
+    mockNavigatorObserver = MockNavigatorObserver();
   });
 
   Future<void> pumpSignInPage(
@@ -23,12 +26,22 @@ void main() {
           home: Builder(builder: (context) {
             return SignInPage.create(context);
           }),
+          navigatorObservers: [mockNavigatorObserver],
         ),
       ),
     );
+    verify(mockNavigatorObserver.didPush(any, any)).called(1);
   }
 
   testWidgets('email & password navigation', (WidgetTester tester) async {
     await pumpSignInPage(tester);
+
+    final emailSignInButton = find.byKey(SignInPage.emailPasswordKey);
+    expect(emailSignInButton, findsOneWidget);
+
+    await tester.tap(emailSignInButton);
+    await tester.pumpAndSettle();
+
+    verify(mockNavigatorObserver.didPush(any, any)).called(1);
   });
 }
